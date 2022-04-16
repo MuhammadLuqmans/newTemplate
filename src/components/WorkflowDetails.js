5af4eeb27471ba8435c74bbe1a62d74027b5dcc2
MuhammadLuqmans <musa60209@gmail.com>
2022-04-16 15:37:32 +0500

    Ready to show

--- src/components/WorkflowDetails.js
+++ src/components/WorkflowDetails.js
@@ -1,7 +1,170 @@
-import React from 'react'
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import { Divider, Grid, Card,  Button } from "@mui/material";
 
-export default function WorkflowDetails() {
+const UserDetails = [
+  {
+    id: 1,
+    UserName: "UserFirstName13...",
+    imgUrl: "",
+    heading: "Approvers",
+    status: "Approved",
+    date: "04 June 2021",
+  },
+  {
+    id: 2,
+    UserName: "UserFirstName13...",
+    imgUrl: "",
+    heading: "Cc Roles",
+  },
+];
+
+export default function WorkFlowModel({ HandleOpen, Open, handleClose }) {
   return (
-    <div>WorkflowDetails</div>
-  )
+    <div>
+      <Modal
+        open={Open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            bgcolor: "background.paper",
+            boxShadow: 24,
+            width: "90%",
+            height: "80%",
+            maxWidth: "md",
+            overflow: "auto",
+            border:'none',
+          }}
+        >
+          <Typography
+            id="modal-modal-title"
+            variant="h4"
+            component="h2"
+            sx={{
+              bgcolor: "hsl(177deg 98% 19%)",
+              textAlign: "center",
+              color: "#fff",
+              py: 2,
+              textShadow: "3px 3px 5px #232323",
+            }}
+          >
+            WorkFlow Participant
+          </Typography>
+          <Box sx={{ px: 4, pt: 5, pb: 2 }}>
+
+          {UserDetails.map((data) => {
+            return (
+              <>
+                <Typography
+                  id="modal-modal-description"
+                  sx={{
+                    mt: 2,
+                    color: "#0f4861",
+                    fontWeight: "bold",
+                    fontSize: 24,
+                  }}
+                >
+                  {data.heading}
+                </Typography>
+                <Divider sx={{ border: "0.5px solid #cbc9c9 ", mt: 1 }} />
+                <Grid
+                  item
+                  container
+                  sx={{ justifyContent: "space-between", mt: 2 }}
+                >
+                  <Grid item container xs={12} sm={12} md={6}>
+                    <Card sx={{ width: 120, height: 120 }}>
+                      <Box
+                        component="img"
+                        src="./assets/img.png"
+                        alt="manDp"
+                        sx={{
+                          bgcolor: "#cfc6c6",
+                          width: "100%",
+                          height: "100%",
+                        }}
+                      />
+                    </Card>
+                    <Typography
+                      id="modal-modal-description"
+                      sx={{
+                        mt: 1,
+                        ml: 2,
+                        color: "#3e3e3e",
+                        fontWeight: "bold",
+                        fontSize: 18,
+                        fontFamily: "Roboto slab, sans-serif",
+                      }}
+                    >
+                      {data.UserName}
+                    </Typography>
+                  </Grid>
+
+                  <Grid item xs={12} sm={12} md={6} sx={{ pl: 2 }}>
+                    <Typography
+                      id="modal-modal-description"
+                      sx={{
+                        mt: 1,
+                        color: "green",
+                        fontWeight: "bold",
+                        fontSize: 18,
+                      }}
+                    >
+                      {data.status}
+                    </Typography>
+                    <Typography
+                      id="modal-modal-description"
+                      sx={{
+                        mt: 1,
+                        color: "gray",
+                        fontSize: 18,
+                      }}
+                    >
+                      {data.date}
+                    </Typography>
+                  </Grid>
+                </Grid>
+                </>
+            );
+          })}
+              </Box>
+          <Box
+            sx={{    
+              textAlign:'right',
+              mt:15,
+            }}
+          >
+                <Divider />
+            
+            <Button
+              variant="contained"
+              onClick={handleClose}
+              sx={{
+                backgroundColor: "hsl(177deg 98% 19%) !important",
+                mr: 3,
+                color: "#fff",
+                width: 130,
+                py: 1,
+                fontWeight: "bold",
+                fontSize: "16px",
+                mt:2
+              }}
+            >
+              Close
+            </Button>
+          </Box>
+        </Box>
+
+      </Modal>
+    </div>
+  );
 }
--- src/components/workflows/Activity.js
+++ src/components/workflows/Activity.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { Grid, Typography, Box, Card } from "@mui/material";
+import WorkFlowModel from "../WorkflowDetails";
 
 export default function Activity() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <Card>
       <Grid item container xs={12} sx={{ mt: 1, p: { xs: 2, sm: 3, md: 3 } }}>
@@ -34,22 +39,24 @@ export default function Activity() {
           md={6}
           lg={8}
           sx={{
-            pl:{ xs:0, sm:2 },
+            pl: { xs: 0, sm: 2 },
             mt: { xs: 3, sm: 0 },
             textAlign: { xs: "center", sm: "left" },
           }}
         >
           <Typography
+            onClick={handleOpen}
             sx={{
-                fontSize: { xs:18, sm:20 },
+              fontSize: { xs: 18, sm: 20 },
               color: "#f9a11c",
+              cursor: "pointer",
             }}
           >
             Sarah1 Employee2
           </Typography>
           <Typography
             sx={{
-                fontSize: { xs:16, sm:18 },
+              fontSize: { xs: 16, sm: 18 },
               color: "#23232",
               fontFamily: "Roboto, sans-serif",
             }}
@@ -58,7 +65,7 @@ export default function Activity() {
           </Typography>
           <Typography
             sx={{
-                fontSize: { xs:16, sm:18 },
+              fontSize: { xs: 16, sm: 18 },
               color: "#232323",
               fontFamily: "Roboto, sans-serif",
             }}
@@ -67,6 +74,13 @@ export default function Activity() {
           </Typography>
         </Grid>
       </Grid>
+      {/* Modal For Activities */}
+      <WorkFlowModel
+        HandleOpen={handleOpen}
+        Open={open}
+        handleClose={handleClose}
+      />
+
       <Typography
         sx={{
           bgcolor: "#01615c",
@@ -109,14 +123,14 @@ export default function Activity() {
           md={6}
           lg={8.5}
           sx={{
-            pl:{ xs:0, sm:2 },
+            pl: { xs: 0, sm: 2 },
             mt: { xs: 3, sm: 0 },
             textAlign: { xs: "center", sm: "left" },
           }}
         >
           <Typography
             sx={{
-              fontSize: { xs:16, sm:18 },
+              fontSize: { xs: 16, sm: 18 },
               color: "#232323",
             }}
           >
@@ -161,29 +175,31 @@ export default function Activity() {
           md={6}
           lg={8.5}
           sx={{
-            pl:{ xs:0, sm:2 },
+            pl: { xs: 0, sm: 2 },
             mt: { xs: 3, sm: 0 },
             textAlign: { xs: "center", sm: "left" },
           }}
         >
           <Typography
             sx={{
-              fontSize: { xs:16, sm:18 },
+              fontSize: { xs: 16, sm: 18 },
               color: "#232323",
               fontFamily: "Roboto, sans-serif",
             }}
           >
-            Initiated by Sara Weston on the behave of Payslip Employ on 04 June 2021
+            Initiated by Sara Weston on the behave of Payslip Employ on 04 June
+            2021
           </Typography>
           <Typography
             sx={{
-              fontSize: { xs:16, sm:18 },
+              fontSize: { xs: 16, sm: 18 },
               color: "#7a7777c4",
               fontFamily: "Roboto, sans-serif",
-              mt:1
+              mt: 1,
             }}
           >
-            "Selected On Probation as on Option for Probation Completed but changed Probation End Date to 3/6"
+            "Selected On Probation as on Option for Probation Completed but
+            changed Probation End Date to 3/6"
           </Typography>
         </Grid>
       </Grid>
